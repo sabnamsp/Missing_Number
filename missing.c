b46f78f7ddbd9e3c0bf681465a07fad1b7ee59e6
Sabnam Parvin <114932621+sabnamsp@users.noreply.github.com>
2023-05-26 11:15:32 +0600

    Update missing.c

--- missing.c
+++ missing.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main() {
+    int t;
+    scanf("%d", &t);
+    while(t--) {
+        unsigned int s, a, b, c;
+        scanf("%u %u %u %u", &s, &a, &b, &c);
+        unsigned int d = s - a - b - c; // find the missing number
+        printf("%u\n", d);
+    }
+    return 0;
+}
